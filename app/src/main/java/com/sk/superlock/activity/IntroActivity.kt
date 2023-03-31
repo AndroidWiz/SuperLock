2c0b0a77136e4614f644ef007861ba8846967603
AndroidWiz <rashedc004@gmail.com>
2023-04-01 02:08:13 +0600

    slider completed

--- app/src/main/java/com/sk/superlock/activity/IntroActivity.kt
+++ app/src/main/java/com/sk/superlock/activity/IntroActivity.kt
@@ -4,97 +4,108 @@ import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.core.content.ContextCompat
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.sk.superlock.R
 import com.sk.superlock.adapter.SliderAdapter
 import com.sk.superlock.databinding.ActivityIntroBinding
 import com.sk.superlock.model.SliderData
-import com.sk.superlock.util.CustomButton
 import com.sk.superlock.util.CustomTextViewBold
 
 @Suppress("DEPRECATION")
 class IntroActivity : BaseActivity() {
 
     private lateinit var binding: ActivityIntroBinding
-    private lateinit var viewpager: ViewPager
-    private lateinit var sliderAdapter : SliderAdapter
-    private lateinit var sliderList: ArrayList<SliderData>
-    private lateinit var btnSkip : CustomButton
-    private lateinit var indicatorSlider1 : CustomTextViewBold
-    private lateinit var indicatorSlider2 : CustomTextViewBold
-    private lateinit var indicatorSlider3 : CustomTextViewBold
+    private lateinit var viewpager: ViewPager2
+    private lateinit var sliderAdapter: SliderAdapter
+    private val sliderList = mutableListOf<SliderData>()
+    private lateinit var indicatorTextViews: List<CustomTextViewBold>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // change status bar color
+//        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
+
         viewpager = binding.introViewPager
-        btnSkip = binding.btnSkipIntro
-        indicatorSlider1 = binding.tvSlide1
-        indicatorSlider2 = binding.tvSlide2
-        indicatorSlider3 = binding.tvSlide3
+        indicatorTextViews = listOf(binding.tvSlide1, binding.tvSlide2, binding.tvSlide3)
 
         // button skip takes to MainActivity
-        btnSkip.setOnClickListener {
+        binding.btnSkipIntro.setOnClickListener {
             startActivity(Intent(this@IntroActivity, MainActivity::class.java))
         }
 
         // add dummy data
-        sliderList = ArrayList()
-//        sliderList.add(SliderData("Key Face PIN", "Set your PIN", R.drawable.ic_password_type))
-//        sliderList.add(SliderData("Key Face face-detection", "Set your app lock using face-ID", R.drawable.ic_face))
-//        sliderList.add(SliderData("Key Face geolocation", "Set your desired parameter to unlock your apps", R.drawable.ic_location))
-        sliderList.add(SliderData("Key Face PIN", "Set your PIN",
-            ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_password_type)?.apply { setColorFilter(ContextCompat.getColor(this@IntroActivity, R.color.white), PorterDuff.Mode.SRC_IN) }!!
-        ))
-        sliderList.add(SliderData("Key Face face-detection", "Set your app lock using face-ID",
-            ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_face)?.apply { setColorFilter(ContextCompat.getColor(this@IntroActivity, R.color.white), PorterDuff.Mode.SRC_IN) }!!
-        ))
-        sliderList.add(SliderData("Key Face geolocation", "Set your desired parameter to unlock your apps",
-            ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_location)?.apply { setColorFilter(ContextCompat.getColor(this@IntroActivity, R.color.white), PorterDuff.Mode.SRC_IN) }!!
-        ))
+        sliderList.addAll(
+            mutableListOf(
+                SliderData(
+                    "Key Face PIN",
+                    "Set your PIN",
+                    ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_password_type)
+                        ?.apply {
+                            setColorFilter(
+                                ContextCompat.getColor(this@IntroActivity, R.color.white),
+                                PorterDuff.Mode.SRC_IN
+                            )
+                        }!!
+                ),
+                SliderData(
+                    "Key Face face-detection",
+                    "Set your app lock using face-ID",
+                    ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_face)?.apply {
+                        setColorFilter(
+                            ContextCompat.getColor(this@IntroActivity, R.color.white),
+                            PorterDuff.Mode.SRC_IN
+                        )
+                    }!!
+                ),
+                SliderData(
+                    "Key Face geolocation",
+                    "Set your desired parameter to unlock your apps",
+                    ContextCompat.getDrawable(this@IntroActivity, R.drawable.ic_location)?.apply {
+                        setColorFilter(
+                            ContextCompat.getColor(this@IntroActivity, R.color.white),
+                            PorterDuff.Mode.SRC_IN
+                        )
+                    }!!
+                )
+            )
+        )
+
 
         // adapter class
-        sliderAdapter = SliderAdapter(this@IntroActivity, sliderList)
+        sliderAdapter = SliderAdapter(this@IntroActivity, sliderList) // modify this line
         viewpager.adapter = sliderAdapter
-        viewpager.addOnPageChangeListener(viewListener)
+        viewpager.registerOnPageChangeCallback(viewListener)
     }
 
-    private var viewListener: ViewPager.OnPageChangeListener = object : ViewPager.OnPageChangeListener{
-        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-
-        override fun onPageSelected(position: Int) {
-            // checking position and updating text view text color.
-            when (position) {
-                0 -> {
-                    indicatorSlider1.setTextColor(resources.getColor(R.color.white))
-                    indicatorSlider2.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                    indicatorSlider3.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-
-                }
-                1 -> {
-                    indicatorSlider1.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                    indicatorSlider2.setTextColor(resources.getColor(R.color.white))
-                    indicatorSlider3.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                }
-                else -> {
-                    indicatorSlider1.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                    indicatorSlider2.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                    indicatorSlider3.setTextColor(resources.getColor(R.color.white))
+    private var viewListener: ViewPager2.OnPageChangeCallback =
+        object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                // checking position and updating text view text color.
+                indicatorTextViews.forEachIndexed { index, textView ->
+                    textView.setTextColor(
+                        ContextCompat.getColor(
+                            this@IntroActivity,
+//                            if (position == index) R.color.white else R.color.colorPrimaryDark
+                            if (position == index) R.color.white else R.color.colorPrimary
+                        )
+                    )
                 }
             }
         }
 
-        override fun onPageScrollStateChanged(state: Int) {}
-
-    }
-
     override fun onResume() {
-        indicatorSlider1.setTextColor(resources.getColor(R.color.white))
-        indicatorSlider2.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-        indicatorSlider3.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-
         super.onResume()
+        indicatorTextViews.forEachIndexed { index, textView ->
+            textView.setTextColor(
+                ContextCompat.getColor(
+                    this@IntroActivity,
+//                    if (viewpager.currentItem == index) R.color.white else R.color.colorPrimaryDark
+                    if (viewpager.currentItem == index) R.color.white else R.color.colorPrimary
+                )
+            )
+        }
     }
 }
--- app/src/main/java/com/sk/superlock/adapter/SliderAdapter.kt
+++ app/src/main/java/com/sk/superlock/adapter/SliderAdapter.kt
@@ -5,44 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.viewpager.widget.PagerAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.sk.superlock.R
 import com.sk.superlock.model.SliderData
+import com.sk.superlock.util.CustomTextView
+import com.sk.superlock.util.CustomTextViewBold
 
-class SliderAdapter(private val context: Context, private val sliderList: ArrayList<SliderData>) : PagerAdapter() {
+class SliderAdapter(private val context: Context, private val sliderList: MutableList<SliderData>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun getCount(): Int {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_intro_slider, parent, false)
+        return SliderViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
         return sliderList.size
     }
 
-    override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        return view === `object` as RelativeLayout
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val sliderViewHolder = holder as SliderViewHolder
+        val sliderData = sliderList[position]
+
+        sliderViewHolder.tvSliderHeading.text = sliderData.slideTitle
+        sliderViewHolder.tvSliderDesc.text = sliderData.slideDescription
+        sliderViewHolder.imgSlider.setImageDrawable(sliderData.slideImage)
     }
 
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-        val view: View = layoutInflater.inflate(R.layout.item_intro_slider, container, false)
-
-        val imgSlider : ImageView = view.findViewById(R.id.iv_slider)
-        val tvSliderHeading: TextView = view.findViewById(R.id.tv_slider_title)
-        val tvSliderDesc: TextView = view.findViewById(R.id.tv_slider_description)
-
-        val sliderData : SliderData = sliderList[position]
-        tvSliderHeading.text = sliderData.slideTitle
-        tvSliderDesc.text = sliderData.slideDescription
-//        imgSlider.setImageResource(sliderData.slideImage)
-        imgSlider.setImageDrawable(sliderData.slideImage)
-
-
-        container.addView(view)
-
-        return view
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as RelativeLayout)
+    inner class SliderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgSlider: ImageView = itemView.findViewById(R.id.iv_slider)
+        val tvSliderHeading: CustomTextViewBold = itemView.findViewById(R.id.tv_slider_title)
+        val tvSliderDesc: CustomTextView = itemView.findViewById(R.id.tv_slider_description)
     }
 }
+
