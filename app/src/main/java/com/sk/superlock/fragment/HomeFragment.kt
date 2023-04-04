eb2ab69b6038f5e266a5a22b1792595489bec36b
AndroidWiz <rashedc004@gmail.com>
2023-04-04 06:16:59 +0600

    new design added to HomeFragment.. camera login work in progress

--- app/src/main/java/com/sk/superlock/fragment/HomeFragment.kt
+++ app/src/main/java/com/sk/superlock/fragment/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.sk.superlock.R
 import com.sk.superlock.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,7 +20,43 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // junk cleaner
+        binding.junkCleaner.setOnClickListener {
+            Toast.makeText(requireContext(), "Junk Cleaner", Toast.LENGTH_SHORT).show()
+        }
+        // network analysis
+        binding.networkAnalysis.setOnClickListener {
+            Toast.makeText(requireContext(), "Network Analysis", Toast.LENGTH_SHORT).show()
+        }
+        // booster
+        binding.booster.setOnClickListener {
+            Toast.makeText(requireContext(), "Booster", Toast.LENGTH_SHORT).show()
+        }
+        // app lock
+        binding.cvAppLock.setOnClickListener {
+            navigateToFragment(ApplicationsFragment())
+        }
+        // memory space
+        binding.cvMemorySpace.setOnClickListener {
+            Toast.makeText(requireContext(), "Memory Space", Toast.LENGTH_SHORT).show()
+        }
+        // storage space
+        binding.cvStorage.setOnClickListener {
+            Toast.makeText(requireContext(), "Storage Space", Toast.LENGTH_SHORT).show()
+        }
+
         return view
+    }
+
+    // change fragment
+    private fun navigateToFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
 }
--- app/src/main/java/com/sk/superlock/fragment/LoginCameraFragment.kt
+++ app/src/main/java/com/sk/superlock/fragment/LoginCameraFragment.kt
@@ -1,16 +1,44 @@
 package com.sk.superlock.fragment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.*
+import android.media.ImageReader
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.sk.superlock.databinding.FragmentLoginCameraBinding
+
 
 class LoginCameraFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginCameraBinding
 
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var cameraCaptureSession: CameraCaptureSession
+
+    private val stateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            if (::cameraDevice.isInitialized) {
+                startCameraPreview()
+            }
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            camera.close()
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            camera.close()
+            activity?.supportFragmentManager?.popBackStack()
+        }
+    }
+
+    @SuppressLint("MissingPermission", "NewApi")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,9 +46,190 @@ class LoginCameraFragment : Fragment() {
         binding = FragmentLoginCameraBinding.inflate(inflater, container, false)
         val view = binding.root
 
+//        requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 1)
+        val requiredPermissions = arrayOf(Manifest.permission.CAMERA)
+        if (requiredPermissions.all { activity?.checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }) {
+            // Permission is granted
+        } else {
+            // Permission is not granted, request it
+            requestPermissions(requiredPermissions, 1)
+        }
+
+        val manager =
+            context?.getSystemService(android.content.Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = manager.cameraIdList[0]
+
+        manager.openCamera(cameraId, stateCallback, null)
+
+        // capture button
+        binding.captureButton.setOnClickListener {
+            takePicture()
+        }
+
+        // cancel button
+        binding.cancelButton.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack()
+        }
 
 
         return view
     }
 
+    // open camera and start preview
+    private fun startCameraPreview() {
+//        val previewSurfaceTexture = binding.cameraPreviewContainer.surfaceTexture
+//        val previewSurfaceTexture = TextureView(requireContext(), binding.cameraPreviewContainer).surfaceTexture
+        val previewSurfaceTexture = TextureView(requireContext()).surfaceTexture
+        previewSurfaceTexture?.setDefaultBufferSize(
+            binding.cameraPreviewContainer.width,
+            binding.cameraPreviewContainer.height
+        )
+        val previewSurface = Surface(previewSurfaceTexture)
+
+        val previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        previewRequestBuilder.addTarget(previewSurface)
+
+        cameraDevice.createCaptureSession(
+            listOf(previewSurface),
+            object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(session: CameraCaptureSession) {
+                    cameraCaptureSession = session
+                    cameraCaptureSession.setRepeatingRequest(
+                        previewRequestBuilder.build(),
+                        null,
+                        null
+                    )
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {
+                    Log.e("LoginCameraFragment", "Failed to create capture session")
+                }
+            },
+            null
+        )
+    }
+
+
+    // take a picture
+    private fun takePicture() {
+        val imageReader = ImageReader.newInstance(
+            binding.cameraPreviewContainer.width,
+            binding.cameraPreviewContainer.height,
+            ImageFormat.JPEG,
+            1
+        )
+        val outputSurfaces = mutableListOf<Surface>()
+        outputSurfaces.add(imageReader.surface)
+
+        val captureRequestBuilder =
+            cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+        captureRequestBuilder.addTarget(imageReader.surface)
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+        val captureCallback = object : CameraCaptureSession.CaptureCallback() {
+            override fun onCaptureCompleted(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                result: TotalCaptureResult
+            ) {
+                super.onCaptureCompleted(session, request, result)
+                val image = imageReader.acquireLatestImage()
+                // process the image
+                image?.close()
+                startCameraPreview()
+            }
+        }
+
+        cameraCaptureSession.stopRepeating()
+        cameraCaptureSession.capture(captureRequestBuilder.build(), captureCallback, null)
+    }
+
+    // request permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // Permission granted
+            } else {
+                // Permission denied
+                activity?.supportFragmentManager?.popBackStack()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::cameraDevice.isInitialized) {
+            cameraDevice.close()
+        }
+        if (::cameraCaptureSession.isInitialized) {
+            cameraCaptureSession.close()
+        }
+    }
+
+
+
 }
+
+/*private fun takePicture() {
+        val imageReader = ImageReader.newInstance(binding.cameraPreviewContainer.width, binding.cameraPreviewContainer.height, ImageFormat.JPEG, 1)
+        val outputSurfaces = mutableListOf<Surface>()
+        outputSurfaces.add(imageReader.surface)
+
+        val captureRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+        captureRequestBuilder.addTarget(imageReader.surface)
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+        val captureCallback = object : CameraCaptureSession.CaptureCallback() {
+            override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
+                super.onCaptureCompleted(session, request, result)
+                val image = imageReader.acquireLatestImage()
+                // process the image
+                image?.close()
+                startCameraPreview()
+            }
+        }
+
+        cameraCaptureSession.stopRepeating()
+        cameraCaptureSession.capture(captureRequestBuilder.build(), captureCallback, null)
+    }*/
+
+/* private fun startCameraPreview() {
+     val previewSurface = binding.cameraPreviewContainer.surfaceTexture
+     camera.startPreview(previewSurface)
+
+     val previewRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+     previewRequestBuilder.addTarget(previewSurface)
+
+     camera.createCaptureSession(listOf(previewSurface), object : CameraCaptureSession.StateCallback() {
+         override fun onConfigured(session: CameraCaptureSession) {
+             cameraCaptureSession = session
+             cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(), null, null)
+         }
+
+         override fun onConfigureFailed(session: CameraCaptureSession) {
+             Log.e("LoginCameraFragment", "Failed to create capture session")
+         }
+     }, null)
+ }*/
+
+
+/*
+override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+}*/
+
+/*   override fun onDestroy() {
+        super.onDestroy()
+        cameraDevice.close()
+        cameraCaptureSession.close()
+    }*/
+
+/*    override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
+            startCameraPreview()
+        }*/
